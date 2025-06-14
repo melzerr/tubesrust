ef282a2cca25ff843a51b7e9eeec78ac1ba129d2
Melzer <imeldaluthfii@gmail.com>
2025-06-14 13:26:09 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+﻿import { defineConfig } from 'vite'
+
+export default defineConfig({
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ['**/src-tauri/**']
+    }
+  }
+})
